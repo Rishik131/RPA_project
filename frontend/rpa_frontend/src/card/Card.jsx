eff38eebf8db9fb5d904fedbd7bab273037543ac
Rishik Kumar <rishik.k2002@gmail.com>
2022-10-03 19:52:18 +0530

    image

--- frontend/rpa_frontend/src/card/Card.jsx
+++ frontend/rpa_frontend/src/card/Card.jsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import './Card.css'
-
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import StarHalfOutlinedIcon from '@mui/icons-material/StarHalfOutlined';
+import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
 
 
 export default function BasicCard() {
   return (
-    <Card sx={{ minWidth: 275, width: 500}}>
+    <Card sx={{ minWidth: 275, width: 500,borderRadius: 3 }} className="card_design">
       <div class="details">
-        Hospital name
+        <div className='name'>Hospital name:</div>
         <br />
-        address
+        <div className='address'>address:</div>
         <br />
-        rating
+        <div className='rating'>rating:<StarOutlinedIcon className='star' fontSize="1px"/>
+          <StarOutlinedIcon className='star' fontSize="1px"/>
+          <StarHalfOutlinedIcon className='star' fontSize="1px"/>
+          <StarOutlineOutlinedIcon className='star' fontSize="1px"/>
+          <StarOutlineOutlinedIcon className='star' fontSize="1px"/>
+        </div>
         <br />
-        review
+        <div className='review'>review:</div>
     </div>
+    {/* <CardContent>
+    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+    </CardContent> */}
     </Card>
   );
 }
